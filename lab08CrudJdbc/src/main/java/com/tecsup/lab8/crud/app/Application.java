e41dea5f2f5edd0fe58a4c60289bac29fa81fb6c
J2298 <job.cantaro@tecsup.edu.pe>
2017-10-04 11:20:26 -0500

    Primer Commit

---
+++ lab08CrudJdbc/src/main/java/com/tecsup/lab8/crud/app/Application.java
@@ -0,0 +1,43 @@
+package com.tecsup.lab8.crud.app;
+
+import com.tecsup.lab8.crud.bean.Employee;
+import com.tecsup.lab8.crud.dao.EmployeeDAO;
+import com.tecsup.lab8.crud.dao.EmployeeDAOImpl;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		EmployeeDAO dao = new EmployeeDAOImpl();
+		
+		int id = -1;
+		
+		id = dao.create("Job", "Cantaro", (double) 1500);
+		
+		Employee emp = dao.findById(id);
+		
+		System.out.println("Create .. " + emp);
+
+		dao.update(id, "Erick ", "Benitez", (double) 1505);
+		
+		emp = dao.findById(id);
+		
+		System.out.println("Update .. " + emp);
+				
+		/*dao.delete(id);
+
+		emp = dao.findById(id);
+
+		System.out.println("Delete .. " + emp);	*/	
+		
+		
+		for (Employee item : dao.findAll()) {
+			
+			System.out.println(item);
+			 
+		};
+		
+	}
+
+}
---
+++ lab08CrudJdbc/src/main/java/com/tecsup/lab8/crud/dao/EmployeeDAO.java
@@ -0,0 +1,44 @@
+package com.tecsup.lab8.crud.dao;
+
+import java.util.ArrayList;
+
+import com.tecsup.lab8.crud.bean.Employee;
+
+public interface EmployeeDAO {
+
+	/**
+	 * 
+	 * @return
+	 */
+	ArrayList<Employee> findAll();
+
+	/**
+	 * 
+	 * @param id
+	 */
+	Employee findById(int id);
+		
+	/**
+	 * 
+	 * @param pFirstname
+	 * @param pLastname
+	 * @param pSalary
+	 */
+	int create(String pFirstname, String pLastname, Double pSalary, Integer pEdad);
+
+	/**
+	 * 
+	 * @param id
+	 */
+	void delete(int id);
+
+	/**
+	 * 
+	 * @param id
+	 * @param pFirstname
+	 * @param pLastname
+	 * @param pSalary
+	 */
+	void update(int id, String pFirstname, String pLastname, Double pSalary, Integer pEdad);
+
+} 
